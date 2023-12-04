1d9c1e22c7190891ac469ed4a1e9fdeef8ec2325
Lehoangminh-dab <71586973+Lehoangminh-dab@users.noreply.github.com>
2023-12-04 15:23:54 +0700

    Small template fix.

---
+++ template-gcc.s
@@ -0,0 +1,23 @@
+#PROBLEM:
+
+.section .data
+  print_format: .string "%d\n"
+  result: .int 30
+#DATA:
+
+.section .text
+.globl main
+  main:
+#SOLUTION:
+
+#BOILERPLATE: PRINT RESULT
+    pushq %rbp
+    movq %rsp, %rbp
+    movl result, %eax
+    movl $print_format, %edi
+    movl %eax, %esi
+    xorl %eax, %eax
+    call printf
+    movl $0, %eax
+    leave
+    ret
