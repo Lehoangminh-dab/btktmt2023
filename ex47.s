f54ae059607152be67d0ae7f3f1e6644bafc74de
Lehoangminh-dab <71586973+Lehoangminh-dab@users.noreply.github.com>
2023-12-04 15:28:41 +0700

    ex47 completed.

--- ex47.s
+++ ex47.s
@@ -1,13 +1,33 @@
+#PROBLEM:
+# int c, a[4]={1,2,3,4}; c=10-(a[3]+a[2]+a[1]); 
+# Expected result: 1
+
 .section .data
   print_format: .string "%d\n"
-  result: .int 30
+  result: .int 
 #DATA:
+  a: .int 1, 2, 3, 4
+  c: .int 
 
 .section .text
 .globl main
-  main:
+main:
 #SOLUTION:
+  # eax = 0; ebx = 1
+  xor %eax, %eax
+  xor %ebx, %ebx
+  inc %ebx
 
+  add $10, %eax
+
+  sub a(, %ebx, 4), %eax # eax = eax - a[ebx]
+  inc %ebx # ebx++
+
+  sub a(, %ebx, 4), %eax # eax = eax - a[ebx]
+  inc %ebx # ebx++
+  
+  sub a(, %ebx, 4), %eax # eax = eax - a[ebx]
+  inc %ebx # ebx++
 #BOILERPLATE: PRINT RESULT
     pushq %rbp
     movq %rsp, %rbp
