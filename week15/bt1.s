dc83e95f9ac1e0897a63ba5bab7ef126aeeaece5
Lehoangminh-dab <71586973+Lehoangminh-dab@users.noreply.github.com>
2023-12-12 14:43:07 +0700

    Completed week15/bt1

--- week15/bt1.s
+++ week15/bt1.s
@@ -7,25 +7,35 @@
 # }
 # Expected: a0 = 0.5, d = 1.5 -> xmm0 = 8.54296875
 .section .data
-  print_format: .string "%d\n"
-  result: .int 30
+  print_format: .string "%lf\n"
+  result: .double 1.5
 #DATA:
-
+n: .int 7
+a0: .double 0.5
+d: .double 1.5
 .section .text
 .globl main
 main:
 #SOLUTION:
+movsd a0, %xmm0
+movsd d, %xmm2
 
+mov n, %ecx
+for:
+  mulsd %xmm2, %xmm0
+  loop for
+
+movsd %xmm0, result
 #BOILERPLATE: STORE RESULT TO PRINTING VARIABLE
   # mov variable, result
 #BOILERPLATE: PRINT RESULT
-  pushq %rbp
-  movq %rsp, %rbp
-  movl result, %eax
-  movl $print_format, %edi
-  movl %eax, %esi
-  xorl %eax, %eax
-  call printf
-  movl $0, %eax
-  leave
-  ret
+pushq %rbp
+movq %rsp, %rbp
+movsd result, %xmm0
+mov $print_format, %edi
+mov $1, %eax
+call printf
+mov $0, %eax
+leave
+ret
+
