435b35972590f1fd8b4c0383899fc07866085dfe
Lehoangminh-dab <71586973+Lehoangminh-dab@users.noreply.github.com>
2023-12-12 21:55:42 +0700

    bt3 in progress

--- week15/bt3.s
+++ week15/bt3.s
@@ -6,25 +6,36 @@
 # for(ecx=0; exc<n; ecx++)xmm0+=a[ecx];
 
 .section .data
-  print_format: .string "%d\n"
-  result: .int 30
+  print_format: .string "%lf\n"
+  result: .double 1.5
 #DATA:
+a: .float 1.1, 2.2, 3.3, 4.4, 5.5
+n: .int 5
 
 .section .text
 .globl main
 main:
 #SOLUTION:
-
+xorps %xmm0, %xmm0
+mov $0, %ecx
+while:
+  cmp %ecx, n
+  jna endwhile
+do:
+  addss a(, %ecx, 4), %xmm0
+  inc %ecx
+endwhile:
+  movsd %xmm0, result
 #BOILERPLATE: STORE RESULT TO PRINTING VARIABLE
   # mov variable, result
 #BOILERPLATE: PRINT RESULT
-  pushq %rbp
-  movq %rsp, %rbp
-  movl result, %eax
-  movl $print_format, %edi
-  movl %eax, %esi
-  xorl %eax, %eax
-  call printf
-  movl $0, %eax
-  leave
-  ret
+pushq %rbp
+movq %rsp, %rbp
+movsd result, %xmm0
+mov $print_format, %edi
+mov $1, %eax
+call printf
+mov $0, %eax
+leave
+ret
+
--- week15/bt4.s
+++ week15/bt4.s
@@ -1,13 +1,9 @@
 # Note: This file's target compiler is gcc.
 #PROBLEM:
-# void calculate(float *pf, double d, int scale)
-# {
-# 	*pf=d*(scale+4);
-# }
 
 .section .data
-  print_format: .string "%d\n"
-  result: .int 30
+  print_format: .string "%lf\n"
+  result: .double 1.5
 #DATA:
 
 .section .text
@@ -18,13 +14,13 @@ main:
 #BOILERPLATE: STORE RESULT TO PRINTING VARIABLE
   # mov variable, result
 #BOILERPLATE: PRINT RESULT
-  pushq %rbp
-  movq %rsp, %rbp
-  movl result, %eax
-  movl $print_format, %edi
-  movl %eax, %esi
-  xorl %eax, %eax
-  call printf
-  movl $0, %eax
-  leave
-  ret
+pushq %rbp
+movq %rsp, %rbp
+movsd result, %xmm0
+mov $print_format, %edi
+mov $1, %eax
+call printf
+mov $0, %eax
+leave
+ret
+
